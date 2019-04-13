2922d1a6ea6caf49a2a5f9e200d0556a43b77492
Maxim Emelyanov <maxcoolzero@gmail.com>
2019-04-13 09:27:00 +0700

    Change browserslist postscc

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-cssnext')({
-      browserslist: [
-        '> 1%',
-        'last 2 versions',
-      ],
-    }),
+    require('postcss-cssnext')(),
   ],
 };
